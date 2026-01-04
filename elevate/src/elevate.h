1a4e9d2b4d93e7816557a610bb708f092aeaffcf
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-04 17:13:22 +0000

    Add brute force and memoization function declarations

--- elevate/src/elevate.h
+++ elevate/src/elevate.h
@@ -4,5 +4,7 @@
 
 int fw(int a, int b, int dests[], int numPeople);
 int Recursive(int stops, int biggest, int dests[], int numPeople);
+void solve_brute(int numStops, int maxFloor, int dests[], int numPeople);
+void solve_memoize(int numStops, int numFloors, int dests[], int n);
 
 #endif
