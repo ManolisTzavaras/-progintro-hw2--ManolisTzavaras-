f5bef3cb5507552e3152d84db67b2e27956e3be7
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-06 13:46:02 +0000

    Replace solve_brute with solve_dp and add new function

--- elevate/src/elevate.h
+++ elevate/src/elevate.h
@@ -4,7 +4,7 @@
 
 int fw(int a, int b, int dests[], int numPeople);
 int Recursive(int stops, int biggest, int dests[], int numPeople);
-void solve_brute(int numStops, int maxFloor, int dests[], int numPeople);
-
+void solve_dp(int numStops, int numFloors, int dests[], int n);
+void Recursive_implementation(int numStops,int numFloors,int dest[], int numPeople );
 
 #endif
