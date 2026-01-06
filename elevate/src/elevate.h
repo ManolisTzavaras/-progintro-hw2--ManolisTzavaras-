5ed5e20d4b590b749b89205a6c5a2f0e6790fe0e
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-06 14:40:14 +0000

    Add Brute and calCost function declarations

--- elevate/src/elevate.h
+++ elevate/src/elevate.h
@@ -1,4 +1,3 @@
-
 #ifndef ELEVATE_H
 #define ELEVATE_H
 
@@ -6,5 +5,8 @@ int fw(int a, int b, int dests[], int numPeople);
 int Recursive(int stops, int biggest, int dests[], int numPeople);
 void solve_dp(int numStops, int numFloors, int dests[], int n);
 void Recursive_implementation(int numStops,int numFloors,int dest[], int numPeople );
+void Brute(int numStops,int numFloors,int dest[], int numPeople );
+int calCost(int list[], int numStops, int dest[], int numPeople);
+
 
 #endif
