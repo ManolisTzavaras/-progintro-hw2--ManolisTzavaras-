80c5c4080e9880b31d0e1304b460d2894accc564
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-08 10:28:56 +0000

    Update elevate.h

--- elevate/src/elevate.h
+++ elevate/src/elevate.h
@@ -1,12 +1,13 @@
 #ifndef ELEVATE_H
 #define ELEVATE_H
 
+// Reference: 6-7
 int fw(int a, int b, int dests[], int numPeople);
 int Recursive(int stops, int biggest, int dests[], int numPeople);
-void solve_dp(int numStops, int numFloors, int dests[], int n);
-void Recursive_implementation(int numStops,int numFloors,int dest[], int numPeople );
-void Brute(int numStops,int numFloors,int dest[], int numPeople );
+void Recursive_implementation(int numStops, int numFloors, int dests[], int numPeople);
+void Memoize_implementation(int numStops, int numFloors, int dests[], int numPeople);
+void solve_dp(int numStops, int numFloors, int dests[], int numPeople, int debug);
+void Brute(int numStops, int numFloors, int dests[], int numPeople);
 int calCost(int list[], int numStops, int dest[], int numPeople);
-
 
 #endif
