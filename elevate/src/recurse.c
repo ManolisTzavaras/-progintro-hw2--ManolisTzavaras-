0e526bea9339a60c900c514b6d898697c61df6a1
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-10 14:55:47 +0000

    Refactor recursive functions for clarity and efficiency

--- elevate/src/recurse.c
+++ elevate/src/recurse.c
@@ -1,117 +1,54 @@
 #include <stdio.h>
 #include "elevate.h"
 
-
-int fw(int a, int b, int dests[], int numPeople)
-{
-
+int fw(int a, int b, int dests[], int numPeople) {
     int cost = 0;
-
-    for(int i = 0; i < numPeople; i++)
-    {
-        if (dests[i] > a && (b == -1 || dests[i] <= b))
-        {
-         
-            if (b == -1)
-            {
-                
-                cost += dests[i] - a;
-
+    for (int i = 0; i < numPeople; i++) {
+        if (dests[i] > a && (b == -1 || dests[i] <= b)) {
+            if (b == -1) {
+                cost += (dests[i] - a);
+            } else {
+                int dist_a = dests[i] - a;
+                int dist_b = b - dests[i];
+                if (dist_a < dist_b) {
+                    cost += dist_a;
+                } else {
+                    cost += dist_b;
+                }
             }
-            else
-            {
-
-                int distance_a = dests[i] - a;
-                int distance_b = b -dests[i];
-
-
-                if (distance_a == distance_b)
-                {
-                    cost += distance_a;
-                }
-                
-
-                else if (distance_a < distance_b)
-                {
-                    
-                    cost += distance_a;
-                }
-                else
-                {
-                    cost += distance_b;
-
-                }
-                
-                
-
-
-            }
-
-
         }
-        
-
-
-        
     }
-
     return cost;
 }
 
-int Recursive(int stops, int biggest, int dests[], int numPeople)
-{
-    if ( stops == 0)
-    {
-        
+int Recursive(int stops, int j, int dests[], int numPeople) {
+    if (stops == 0) {
         return fw(0, -1, dests, numPeople);
-
     }
-
     int min_cost = -1;
-
-    for(int k = 0; k <= biggest; k++)
-    {
-
-        int cost = Recursive(stops - 1, k, dests, numPeople) - fw(k , -1, dests, numPeople) + fw(k, biggest, dests, numPeople) + fw(biggest, -1, dests, numPeople);
-
-        if (cost < min_cost || min_cost == -1)
-        {
+    for (int k = 0; k <= j; k++) {
+        int cost = Recursive(stops - 1, k, dests, numPeople) - fw(k, -1, dests, numPeople) + fw(k, j, dests, numPeople) + fw(j, -1, dests, numPeople);
+        if (min_cost == -1 || cost < min_cost) {
             min_cost = cost;
         }
-        
-
     }
-    
     return min_cost;
-
 }
-void Recursive_implementation(int numStops,int numFloors,int dests[], int numPeople)
-{
 
-    if (numStops > 0)
-    {
-        int minCosts = -1;
-        int LastFloor = 0;
-
-        for (int biggest = 0; biggest <= numFloors; biggest++)
-        {
-            int cost = Recursive(numStops, biggest, dests, numPeople);
-
-            if (minCosts == -1 || cost < minCosts)
-            {
-                minCosts = cost;
-                LastFloor = biggest;
-                
-            }
-            
-
+void Recursive_implementation(int numStops, int numFloors, int dests[], int numPeople) {
+    int minCost = -1;
+    int lastFloor = 0;
+    for (int j = 0; j <= numFloors; j++) {
+        int cost = Recursive(numStops, j, dests, numPeople);
+        if (minCost == -1 || cost < minCost) {
+            minCost = cost;
+            lastFloor = j;
         }
-        
-        printf("Last stop at floor: %d\n", LastFloor );
-        printf("The minimum cost is: %d\n", minCosts);
-
-
     }
-    
-    
+    if (minCost == fw(0, -1, dests, numPeople)) {
+        printf("No lift stops\n");
+    } else {
+        printf("Last stop at floor: %d\n", lastFloor);
+    }
+    printf("The minimum cost is: %d\n", minCost);
 }
