3471f426281b6fdefbc684f45b1749026e759269
ManolisTzavaras <emanstzavaras@gmail.com>
2026-01-10 14:54:32 +0000

    Refactor main function and improve argument handling

--- elevate/src/elevate.c
+++ elevate/src/elevate.c
@@ -1,85 +1,69 @@
 #include <stdio.h>
 #include <string.h>
-#include "elevate.h"
 #include <stdlib.h>
+#include "elevate.h"
 
-int main(int argc, char * argv[]){
-
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
 
     FILE *file = fopen(argv[1], "r");
-
-    if (!file)
-    {
-        perror("fopen");
+    if (file == NULL) {
         return 1;
     }
 
-    int numPeople;
-    int numStops;
-
-    if (fscanf(file, "%d", &numPeople) !=1)
-    {
-        printf("Failed to read number of people \n");
+    int numPeople, numStops;
+    if (fscanf(file, "%d %d", &numPeople, &numStops) != 2) {
+        fclose(file);
         return 1;
     }
 
-    if (fscanf(file, "%d", &numStops) !=1)
-    {
-        printf("Failed to read number of stops \n");
+    int *dests = malloc(numPeople * sizeof(int));
+    if (dests == NULL) {
+        fclose(file);
         return 1;
     }
 
-    int *pinakas = malloc(numPeople * sizeof(int));
-    
-    if (pinakas == NULL)
-    {
-        fprintf(stderr, "Den uparxei mnimi");
-        return 1;
-    }
-    
-    int dest;
-    int numFloors = -1;
-
-    for (int i = 0; i < numPeople; i++)
-    {
-        if(fscanf(file, "%d", &dest) !=1)
-        {
-            printf("failed to reaf destination: %d\n", i);
-            free(pinakas);
-            return 1;
-
+    int numFloors = 0;
+    for (int i = 0; i < numPeople; i++) {
+        if (fscanf(file, "%d", &dests[i]) != 1) {
+            break;
         }
- 
-        if (dest > numFloors || numFloors < 0)
-        {
-            numFloors = dest; 
-
+        if (dests[i] > numFloors) {
+            numFloors = dests[i];
         }
-        
-        pinakas[i] = dest;
-
-
     }
-    
-
-
     fclose(file);
 
-    if (argc > 2)
-    {
-        if (!strcmp(argv[2], "--mode=recurse"))
-        {
-            
-            Recursive_implementation( numStops, numFloors, pinakas , numPeople);
-            
-
+    char *mode = "dp";
+    int debug = 0;
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "--mode=recurse") == 0) {
+            mode = "recurse";
+        } else if (strcmp(argv[i], "--mode=brute") == 0) {
+            mode = "brute";
+        } else if (strcmp(argv[i], "--mode=memoize") == 0) {
+            mode = "memoize";
+        } else if (strcmp(argv[i], "--mode=dp") == 0) {
+            mode = "dp";
+        }
+        
+        if (strcmp(argv[i], "--debug") == 0) {
+            debug = 1;
         }
     }
-    
 
- 
-    
-    
+    if (strcmp(mode, "recurse") == 0) {
+        Recursive_implementation(numStops, numFloors, dests, numPeople);
+    } else if (strcmp(mode, "brute") == 0) {
+        Brute(numStops, numFloors, dests, numPeople);
+    } else if (strcmp(mode, "memoize") == 0) {
+        Memoize_implementation(numStops, numFloors, dests, numPeople);
+    } else {
+        solve_dp(numStops, numFloors, dests, numPeople, debug);
+    }
 
-    return 0;    
+    free(dests);
+    return 0;
 }
